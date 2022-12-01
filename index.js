d5215f1d00d8614d412cf442246009414d006085
mohammad kaif <mohammadkaif051197@gmail.com>
2022-12-01 09:36:00 +0530

    keep moving

--- index.js
+++ index.js
@@ -35,23 +35,23 @@ app.post('/addStudent', (req, res) => {
 app.put('/changeFeestatus/:id', (req, res) => {
     db.collection('School').updateOne({ schoolCode: req.params.id }, {
         $set: {
-            "feesPaid" : req.body.feestatus
+            "feesPaid": { $not: "$feesPaid" }
         }
     }, (err, result) => {
         if (err) throw err;
         res.send(result);
     });
 });
-    //Connection with db
-    // ! JustExtraApiDataBase  School
-    MongoClient.connect(mongoUrl, (err, client) => {
-        if (err) console.log(`Error While Connecting`);
-        db = client.db('JustExtraApiDataBase');
-        app.listen(port, (err) => {
-            if (err) throw err;
-            console.log(`Express Server listening on port ${port}`)
-        });
+//Connection with db
+// ! JustExtraApiDataBase  School
+MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) console.log(`Error While Connecting`);
+    db = client.db('JustExtraApiDataBase');
+    app.listen(port, (err) => {
+        if (err) throw err;
+        console.log(`Express Server listening on port ${port}`)
     });
+});
 
 
 /*
